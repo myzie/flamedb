78ab3ecf018dcb2fcd1238372195311238fff926
Curtis Myzie <curtis.myzie@gmail.com>
2018-10-06 08:05:30 -0400

    Update ID entropy to be monotonic within same ms

--- database/id.go
+++ database/id.go
@@ -1,16 +1,18 @@
 package database
 
 import (
+	"io"
 	"math/rand"
 	"time"
 
 	"github.com/oklog/ulid"
 )
 
-var entropy *rand.Rand
+var entropy io.Reader
 
 func init() {
-	entropy = rand.New(rand.NewSource(time.Now().UnixNano()))
+	t := time.Now()
+	entropy = ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
 }
 
 // NewID returns a new unique identifier
